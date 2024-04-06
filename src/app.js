f5021883fe60d14b058ff4a490652746df708c91
JohnnatanV <joh.villaneda@outlook.com>
2024-04-06 17:21:24 -0500

    Finished proyect

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
-const main = () => {
-  result();
+document.addEventListener("DOMContentLoaded", fetchData);
+
+async function fetchData() {
   const cardId = document.getElementById("cardTitle");
   const cardQuote = document.getElementById("cardQuote");
   const cardBtn = document.getElementById("cardBtn");
 
-  function result() {
-    const results = fetch("https://api.adviceslip.com/advice");
-
-    results
-      .then((res) => res.json())
-      .then((response) => {
-        const data = response.slip;
-
-        cardId.innerText = `Advice #${data.id}`;
-        cardQuote.innerText = data.advice;
-      });
+  async function result() {
+    let url = "https://api.adviceslip.com/advice";
+    const urlData = await fetch(url);
+    const response = await urlData.json();
+    const data = await response.slip;
+    console.log(response);
+    if (data.id === parseInt(cardId.innerText.slice(8))) {
+      result();
+    } else {
+      cardId.innerText = `advice #${data.id}`;
+      cardQuote.innerText = `"${data.advice}"`;
+    }
   }
-
-  cardBtn.addEventListener("click", result);
-};
-
-document.addEventListener("DOMContentLoaded", main);
+  cardBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    result();
+  });
+  result();
+}
